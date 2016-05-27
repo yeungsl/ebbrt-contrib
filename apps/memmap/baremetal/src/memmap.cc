b06a41be4f715fc6d436f28fda74b134359157ff
David Yeung <yeungsl@bu.edu>
2016-05-27 16:50:55 -0400

    fixed bug, memmap can map arbitrary page sizes

--- apps/memmap/baremetal/src/memmap.cc
+++ apps/memmap/baremetal/src/memmap.cc
@@ -8,32 +8,42 @@
 #include <ebbrt/VMem.h>
 #include <ebbrt/PageAllocator.h>
 #include <ebbrt/MemMap.h>
+#include <ebbrt/PMem.h>
+
 
 class NewPageFaultHandler : public ebbrt::VMemAllocator::PageFaultHandler {
   ebbrt::Pfn page_;
+  uint64_t pageLen;
 public:
-  void setPage(ebbrt::Pfn page) {
+  void setPage(ebbrt::Pfn page, uint64_t length) {
     page_ = page;
+    pageLen = length;
   }
   void HandleFault(ebbrt::idt::ExceptionFrame* ef, uintptr_t faulted_address) override {
     auto vpage = ebbrt::Pfn::Down(faulted_address);
-    ebbrt::vmem::MapMemory(vpage, page_);
+    ebbrt::vmem::MapMemory(vpage, page_, pageLen);
 
   }
 };
 
 void AppMain() { 
-  //  const npages = 2;
+  int l = 3;
+  int mul = 3;
+  auto ps = ebbrt::pmem::kPageSize;
+  int iteration = ((int)ps*(1<<l))/4;
+  int v = 0xDEADBEEF;
+
+
   printer->Print("MEMMAP BACKEND UP.\n"); 
   printer->Print("START TO ALLOCATE 1 PHYSICAL PAGES.\n");
-  auto pfn = ebbrt::page_allocator->Alloc(0);
+  auto pfn = ebbrt::page_allocator->Alloc(l);
   auto pptr = (volatile uint32_t *)pfn.ToAddr();
 
   ebbrt::kprintf("physical address allocated 0x%lx\n", (uintptr_t)pptr);
   printer->Print("FILLING UP THE ADDRESS\n");
-  for(int i = 0; i < 1024; i++){
-    *pptr = 3735928559;
-    
+  ebbrt::kprintf("interations: %i\n", iteration);
+  for(int i = 0; i < iteration; i++){
+    *pptr = v;
     //ebbrt::kprintf("i: %d, address 0x%lx has value 0x%lx\n",i, (uintptr_t)pptr, *pptr);
     pptr++;
   }
@@ -41,16 +51,20 @@ void AppMain() {
   printer->Print("START TO ALLOCATE VIRTUAL PAGE.\n");
   //dont know how to insert page fault handler
   auto pf = std::make_unique<NewPageFaultHandler>();
-  pf->setPage(pfn);
-  auto vfn = ebbrt::vmem_allocator->Alloc(3, std::move(pf));
+  pf->setPage(pfn, ps*(1<<l));
+  auto vfn = ebbrt::vmem_allocator->Alloc(mul, std::move(pf));
 
   auto addr = vfn.ToAddr();
   auto ptr = (volatile uint32_t *)addr;
   ebbrt::kprintf("Virtual address allocated 0x%lx\n", (uintptr_t)ptr);
-  for(int i = 1; i < 3 * 1024; i++){
-    auto val = *ptr;
-    ebbrt::kprintf("Read value: 0x%lx\n", val);  
+  auto counter = 0;
+  for(int i = 0; i < mul * iteration; i++){
+    volatile auto val = *ptr;
+    //ebbrt::kprintf("Read value: 0x%lx\n", val);  
+    val++;
     ptr++;
+    counter++;
   }
+  ebbrt::kprintf("final iteration of the virtual region: %i\n", counter);
 }
 
